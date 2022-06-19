c2b05727870972326d2cc60e0c51b4b23aa3b33d
Annetkhavere <anneywan5@gmail.com>
2022-06-19 15:51:16 +0300

    add a postform class

--- hood/forms.py
+++ hood/forms.py
@@ -25,3 +25,8 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ('user', 'neighbourhood')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
